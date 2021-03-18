f82e73b687c4c720ca9dcc5000b28fdc204df513
RaduLecca-codingStuff <74616093+RaduLecca-codingStuff@users.noreply.github.com>
2021-03-18 20:10:22 +0100

    Added folders and updated code for easier debugging

---
+++ DoubleClick/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static KeyCode Lkey=KeyCode.Mouse0;
+    public static KeyCode Rkey = KeyCode.Mouse1;
+
+    public static float karma = 50;
+    public float money = 0;
+    // Start is called before the first frame update
+    public static void setButtons(KeyCode l,KeyCode r)
+    {
+        Lkey = l;
+        Rkey = r;
+    }
+}
---
+++ DoubleClick/Assets/Scripts/SetButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetButtons : MonoBehaviour
+{
+    public KeyCode lkey;
+    public KeyCode rkey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.setButtons(lkey, rkey);
+    }
+
+ 
+}
