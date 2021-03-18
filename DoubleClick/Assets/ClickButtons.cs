ebfc4e87f9ca051e1dc4c5c65421c0d260d4d758
RaduLecca-codingStuff <74616093+RaduLecca-codingStuff@users.noreply.github.com>
2021-03-18 19:58:15 +0100

    Added a main menu & a game manager
    Alongside that , I also added a small script which can be used to set up the controls for the entire game

---
+++ DoubleClick/Assets/ClickButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image leftPrompt;
+    Image rightPrompt;
+    void Start()
+    {
+        leftPrompt = GameObject.Find("leftPrompt").GetComponent<Image>();
+        rightPrompt = GameObject.Find("rightPrompt").GetComponent<Image>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(GameManager.Lkey))
+        {
+            //this is where the game exits
+        }
+        if (Input.GetKeyDown(GameManager.Rkey))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
---
+++ DoubleClick/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static KeyCode Lkey;
+    public static KeyCode Rkey;
+
+    public static float karma = 50;
+    public float money = 0;
+    // Start is called before the first frame update
+    public static void setButtons(KeyCode l,KeyCode r)
+    {
+        Lkey = l;
+        Rkey = r;
+    }
+}
