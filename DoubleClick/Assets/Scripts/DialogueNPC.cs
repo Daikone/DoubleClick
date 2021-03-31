d95f3e6f71f7063250c86745a3b69607829bc2e4
RaduLecca-codingStuff <74616093+RaduLecca-codingStuff@users.noreply.github.com>
2021-03-31 12:17:25 +0200

    50% done dialog system

--- DoubleClick/Assets/Scripts/DialogueNPC.cs
+++ DoubleClick/Assets/Scripts/DialogueNPC.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DialogueNPC : MonoBehaviour
 {
     public string dialogue;
     private DialogueScript dMan;
     public string[] dialogueLines;
     public string[] dialogueNames;
+    public Sprite Image;
+    public Image portrait;
     
+    public Object OptionVisuals;
     // Start is called before the first frame update
     void Start()
     {
         dMan = FindObjectOfType<DialogueScript>();
         
+        
+        portrait.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(GameManager.canMove);
+        Debug.Log(dMan.currentLine);
+        if (dMan.currentLine > 0)
+            portrait.gameObject.SetActive(true);
+        else
+            portrait.gameObject.SetActive(false);
     }
 
     private void OnTriggerStay(Collider other)
@@ -31,16 +40,14 @@ public class DialogueNPC : MonoBehaviour
 
             if (Input.GetKeyUp(GameManager.Rkey) )
                 {
-                GameManager.canMove = false;
-                
-                   
+                    GameManager.canMove = false;
                     dMan.dialogLines = dialogueLines;
                     dMan.dialogNames = dialogueNames;
                     dMan.ShowDialogue();
                 
-                    
 
-                }
+
+            }
             
             
 
@@ -56,7 +63,9 @@ public class DialogueNPC : MonoBehaviour
             dMan.dialogNames = dialogueNames;
             dMan.currentLine = 0;
             dMan.ShowDialogue();
-            
+            portrait.sprite = Image;
+
+
         }
     }
     private void OnTriggerExit(Collider other)
@@ -64,7 +73,7 @@ public class DialogueNPC : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             dMan.HideDialogue();
-            
+           
         }
     }
 }
--- DoubleClick/Assets/Scripts/GameManager.cs
+++ DoubleClick/Assets/Scripts/GameManager.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
+{//the keys used in the whole project
     public static KeyCode Lkey=KeyCode.Mouse0;
     public static KeyCode Rkey = KeyCode.Mouse1;
-
-    public static float karma = 50;
-    public float money = 0;
     public static bool canMove = true;
+    public static bool optionAvailable = false;
+
+    //money and other stuf
+    public float money = 0;
+    
     // Start is called before the first frame update
     public static void setButtons(KeyCode l,KeyCode r)
     {
