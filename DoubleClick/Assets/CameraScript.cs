fba73eea4f97fb44ba7f6708840e620c261e5a3e
K.Ivanov <>
2021-06-08 17:37:41 +0300

    Berlin and Venice levels + Main Menu

---
+++ DoubleClick/Assets/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public SpriteRenderer background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float orthoSize = background.bounds.size.x * Screen.height / Screen.width * 0.5f;
+        Camera.main.orthographicSize = orthoSize; 
+    }
+
+}
