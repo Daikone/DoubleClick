a53532e6704786f7c12edac6c9a2f8db45dd2376
RaduLecca-codingStuff <74616093+RaduLecca-codingStuff@users.noreply.github.com>
2021-06-09 09:37:09 +0200

    Freeplay fixes

---
+++ DoubleClick/Assets/Scripts/Puzzles/FreeplayTiles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FreeplayTiles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ST_PuzzleDisplay puzzle;
+
+    public Texture[] PossibleImages;
+
+
+    void Start()
+    {
+        if (PossibleImages.Length > 0)
+        {
+            int rInt = Random.Range(0, PossibleImages.Length);
+            puzzle.PuzzleImage = PossibleImages[rInt];
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puzzle.Complete)
+            SceneManager.LoadScene("FreePlayMenu");
+        
+    }
+}
